32d17976f4dd349f57047abf43ea1596754a3048
bogdi xameli <121572305+gmsugff@users.noreply.github.com>
2026-09-17 12:36:42 +0300

    Update DBandTb.sql

---
+++ DBandTb.sql
@@ -0,0 +1,15 @@
+CREATE DATABASE top_top;
+
+CREATE TABLE calc (
+    id SERIAL PRIMARY KEY,
+    a INTEGER,
+    b INTEGER,
+    c INTEGER
+);
+
+CREATE TABLE minus (
+    id SERIAL PRIMARY KEY,
+    a INTEGER,
+    b INTEGER,
+    dif INTEGER
+);
